48ec38c801dc9b3a18a70e38deae0727eb18ebf2
Adriel Pereira <adriel.pemi@gmail>
2022-12-28 12:49:32 -0400

    Reestrutura arquivos

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css';
+
+export default function Home() {
+
+
+    return (
+      <div className="container">
+        <div className="barraMenu">        
+          <h1 className="textoBarraMenu">HOME</h1>      
+        </div>
+          
+      </div>
+    );
+  }
+
